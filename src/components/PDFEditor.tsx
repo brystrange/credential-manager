490d7759e7015d64d9fff602d8704d33e308af48
brystrange <bryan1_mayor@yahoo.com>
2026-07-18 20:06:14 +0800

    Fix PDF viewer: bundle worker as static file served from same origin

--- src/components/PDFEditor.tsx
+++ src/components/PDFEditor.tsx
@@ -5,10 +5,7 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import './PDFEditor.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.mjs';
 
 interface PDFEditorProps {
   url: string | { data: Uint8Array };
